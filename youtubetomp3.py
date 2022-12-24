e2a2b33417f987b49869f6b9046e4c1deb5457f6
Bassam <basmaym148@gmail.com>
2022-12-23 20:03:01 -0500

    added executable

--- youtubetomp3.py
+++ youtubetomp3.py
@@ -1,5 +1,5 @@
 from pytube import YouTube, Playlist
-import os
+from os import rename, environ, path
 
 videoArray = ['video', 'v', 'V', 'Video', 'VIDEO', 'Vid', 'vid', 'a video', 'a Video', 'A video', 'A Video']
 
@@ -7,10 +7,10 @@ if str(input("Am I downloading a single video or a playlist (public or unlisted)
     url = str(input('What is the url of the video? '))
     try:
         video = YouTube(url).streams.filter(only_audio=True).first()
-        Desktop = os.environ["HOMEPATH"] + '\Desktop\\' + str(input("Where should I place the downloaded file? "))
+        Desktop = environ["HOMEPATH"] + '\Desktop\\' + str(input("Where should I place the downloaded file? "))
         download = video.download(output_path=Desktop)
-        name, extension = os.path.splitext(download)
-        os.rename(download, f'{name}.mp3')
+        name, extension = path.splitext(download)
+        rename(download, f'{name}.mp3')
         input('I have successfully downloaded the video. Thank you.')
     except:
         input('That is not a valid url. Please try again.')
@@ -19,12 +19,12 @@ else:
     url = str(input('What is the url of the playlist? '))
     try:
         playlist = Playlist(url)
-        Desktop = os.environ["HOMEPATH"] + '\Desktop\\' + str(input("Where should I place the downloaded files? "))
+        Desktop = environ["HOMEPATH"] + '\Desktop\\' + str(input("Where should I place the downloaded files? "))
         for video_url in playlist:
             video = YouTube(video_url).streams.filter(only_audio=True).first()
             download = video.download(output_path=Desktop)
-            name, extension = os.path.splitext(download)
-            os.rename(download, f'{name}.mp3')
+            name, extension = path.splitext(download)
+            rename(download, f'{name}.mp3')
             print(f"Video '{video.title}' successfully downloaded!")
         input('Playlist successfully downloaded. Thank you.')
     except:
